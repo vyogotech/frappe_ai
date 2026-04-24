45575162838440107465632db24ad959702c359b
Sarathi Thirumalai Soundararajan <tssarathi5901@gmail.com>
2026-04-24 15:08:27 +1000

    refactor(ChatSidebar): wire busy + stop; drop legacy loading props

--- frontend/src/components/ChatSidebar.vue
+++ frontend/src/components/ChatSidebar.vue
@@ -4,7 +4,7 @@ import ChatHeader from "./ChatHeader.vue";
 import ChatMessages from "./ChatMessages.vue";
 import ChatInput from "./ChatInput.vue";
 
-const props = defineProps<{
+defineProps<{
   sidebarWidth: number;
   keyboardShortcut: string;
   visible: boolean;
@@ -12,10 +12,21 @@ const props = defineProps<{
 
 const emit = defineEmits<{ close: [] }>();
 
-const { messages, isLoading, sendMessage, clearMessages } = useChat();
+const {
+  messages,
+  isLoading,
+  canCancel,
+  sendMessage,
+  cancelMessage,
+  clearMessages,
+} = useChat();
 
 function handleSend(content: string) {
   sendMessage(content);
+}
+
+function handleStop() {
+  cancelMessage();
 }
 
 function handleClear() {
@@ -33,15 +44,13 @@ function handleClose() {
     class="frappe-ai-sidebar"
     :style="{ width: sidebarWidth + 'px' }"
   >
-    <ChatHeader
-      :is-loading="isLoading"
-      @clear="handleClear"
-      @close="handleClose"
-    />
-    <ChatMessages :messages="messages" :is-streaming="isLoading" />
+    <ChatHeader @clear="handleClear" @close="handleClose" />
+    <ChatMessages :messages="messages" />
     <ChatInput
-      :disabled="isLoading"
+      :busy="isLoading"
+      :can-cancel="canCancel"
       @send="handleSend"
+      @stop="handleStop"
     />
   </div>
 </template>
